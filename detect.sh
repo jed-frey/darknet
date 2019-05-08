98c8a494ec4eb31597b9fa992fc704a55ab51294
root <root+make_sandwich@8600k-LINUX>
2019-05-08 03:48:26 -0400

    .

--- detect.sh
+++ detect.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 INPUT=`realpath ${1}`
+YOLOV3=${INPUT}.yolov3
 
+if [ -f "${YOLOV3}" ]; then
+    echo "Skipping ${INPUT}"
+else
 cd /root/.darknet
 ./darknet detect cfg/yolov3.cfg weights/yolov3.weights "$1" 2>/dev/null >${INPUT}.yolov3
+fi
